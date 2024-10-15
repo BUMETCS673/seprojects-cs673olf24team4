3038409f192ac362e40a24bb68d8290f95eb977c
vishal <vishal2@bu.edu>
2024-10-15 07:03:19 +0530

    view teams by selecting group id; add slide toggle for admin login

--- code/team-builder-web/src/app/home-page/home-page.component.ts
+++ code/team-builder-web/src/app/home-page/home-page.component.ts
@@ -5,13 +5,31 @@ import { MatDialog } from '@angular/material/dialog';
 import { AdminLoginDialogComponent } from '../admin-login-dialog/admin-login-dialog.component';
 import { AuthService } from '../service/auth.service';
 import { ActivatedRoute } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { GroupService } from '../service/group.service';
+import { ToastrService } from 'ngx-toastr';
+import { Group } from '../model/group.model';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Team } from '../model/team.model';
+import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'home-page',
   standalone: true,
   imports: [
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatListModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatDividerModule
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
@@ -21,7 +39,15 @@ export class HomePageComponent {
   readonly dialog = inject(MatDialog);
   readonly authService = inject(AuthService);
 
-  constructor(private route: ActivatedRoute) {}
+  groups: Group[] = [];
+  teams: Team[] = []
+  selectedGroup = new FormControl();
+
+  constructor(
+    private route: ActivatedRoute,
+    private groupService: GroupService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
@@ -29,6 +55,12 @@ export class HomePageComponent {
         console.error('You must log in as admin to access that page.');
       }
     });
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id && id.length > 0) {
+      this.selectedGroup.setValue(id);
+    } else {
+      this.loadAllGroups();
+    }
   }
 
   openDialog(): void {
@@ -40,6 +72,29 @@ export class HomePageComponent {
     dialogRef.afterClosed().subscribe(result => {
       // if needed 
     });
+  }
+
+  loadAllGroups() {
+    this.groupService.getAllGroups().subscribe({
+      next: (data) => {
+        this.groups = data;
+      },
+      complete: () => { 
+        // if needed
+      },
+      error: () => { 
+        this.toastr.error('Error saving user'); 
+      }
+    });
+  }
+
+  onGroupSelect(): void {
+    if (this.selectedGroup.value) {
+      // Fetch teams for the selected group
+      this.groupService.getTeamsByGroupId(this.selectedGroup.value).subscribe(teams => {
+        this.teams = teams;
+      });
+    }
   }
 
 }
--- code/team-builder-web/src/app/sidenav/sidenav.component.ts
+++ code/team-builder-web/src/app/sidenav/sidenav.component.ts
@@ -3,6 +3,10 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../service/auth.service';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatDialog } from '@angular/material/dialog';
+import { AdminLoginDialogComponent } from '../admin-login-dialog/admin-login-dialog.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'sidenav',
@@ -14,9 +18,41 @@ import { AuthService } from '../service/auth.service';
     MatListModule,
     RouterOutlet, 
     RouterLink, 
-    RouterLinkActive
+    RouterLinkActive,
+    MatSlideToggleModule,
+    FormsModule
   ]
 })
 export class SidenavComponent {
   readonly authService = inject(AuthService);
+
+  adminToggle: boolean = false;
+
+  constructor(public dialog: MatDialog) {
+    this.adminToggle = this.authService.isLoggedIn();
+  }
+
+  toggleAdminLogin(): void {
+    if (this.adminToggle) {
+      this.openAdminLoginDialog();
+    } else {
+      this.authService.clearCredentials(); // logout
+    }
+  }
+
+  openAdminLoginDialog(): void {
+    const dialogRef = this.dialog.open(AdminLoginDialogComponent, {
+      width: '400px',
+      disableClose: true
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (this.authService.isLoggedIn()) {
+        this.adminToggle = true;
+      } else {
+        this.adminToggle = false;
+      }
+    });
+  }
+
 }
